ddb6743cb2df40f1c38101af2eb7d3527ce68757
Andrey <olgi83@Yandex.ru>
2023-11-08 21:43:07 +0300

    1 задание

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello Js')
+//Формула вычисления расстояния между двумя точками A(xa, ya) и B(xb, yb) на плоскости:AB = √(xb - xa)2 + (yb - ya)2 
+
+function calculateLong(xa, ya, xb, yb) {
+  let a = xb - xa;
+  let b = yb - ya;
+  return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+}
+let result = calculateLong(0, 1, 2, -2);
+console.log(result);
