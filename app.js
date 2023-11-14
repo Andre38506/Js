36b2da4b04b4db77b806691f4b1e011797aa3870
Andrey <olgi83@Yandex.ru>
2023-11-14 21:53:03 +0300

    аы

--- app.js
+++ app.js
@@ -5,18 +5,25 @@ console.log('Hello Js');
 И возвращает отфильтрованный массив. При этом функция удаления элементов принимает
 число и возвращает true, если его надо удалить и false, если надо оставить */
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8];
-const filterAray = (arr, fn, num) => {
-  const newArr = [];
-  for (const el of arr) {
-    if (fn(el, num)) {
-      continue;
-    } else {
-      newArr.push(el);
-    }
-  }
-  return newArr;
-};
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// const filterAray = (arr, fn, num) => {
+//   const newArr = [];
+//   for (const el of arr) {
+//     if (fn(el, num)) {
+//       continue;
+//     } else {
+//       newArr.push(el);
+//     }
+//   }
+//   return newArr;
+// };
 
-const isNum = (el, num) => el <= num;
-console.log(filterAray(arr, isNum, 5));
+// const isNum = (el, num) => el <= num;
+// console.log(filterAray(arr, isNum, 5));
+
+const price = [[100, 200], [120, 100], [200, 350]];
+
+const fiterArr = price.filter(priceEl => priceEl[0] < priceEl[1])
+  .map(el => el[1] - el[0]);
+  
+console.log(fiterArr);
